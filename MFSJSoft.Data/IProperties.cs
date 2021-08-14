5eb1d1b291bb7b6c4706dd5947e4a110ec5c72ae
Martin Schlegel <martin.schlegel.1838@outlook.com>
2021-08-14 16:13:32 -0700

    Documentation test.

--- MFSJSoft.Data/IProperties.cs
+++ MFSJSoft.Data/IProperties.cs
@@ -1,8 +1,16 @@
 ﻿
 namespace MFSJSoft.Data
 {
+	/// <summary>
+	/// IProperties represents a generic read-only accessor to string-valued properties.
+	/// </summary>
     public interface IProperties
     {
+		
+		/// <summary>
+		/// Returns the value of the property matching the given <c>name</c>. If not found,
+		/// return <see langword="null" />
+		/// </summary>
         string GetProperty(string name);
     }
 }
